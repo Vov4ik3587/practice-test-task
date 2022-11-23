8c905e4bcb1f00ced9605c573524f0cc0e4296b1
Vov4ik3587 <galstyan.vova@gmail.com>
2022-11-24 01:31:57 +0700

    Создается таблица

--- sheets.py
+++ sheets.py
@@ -1,35 +1,73 @@
-import os
 from pprint import pprint
 
 import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 
-CREDENTIALS_FILE = 'creds.json'
 
-class GSheet:
+def create_sheet(service, info, httpAuth):
+    new_spreadsheet = service.spreadsheets().create(body={
+        'properties': {'title': info, 'locale': 'ru_RU'},
+        'sheets': [{'properties': {
+            'sheetType': 'GRID',
+            'sheetId': 0,
+            'title': 'Главное',
+            'gridProperties': {'rowCount': 500, 'columnCount': 10}}}]
+    }).execute()
+    pprint(f"URL созданной таблицы {new_spreadsheet['spreadsheetUrl']}")
 
-    
+    # Выдача доступа на чтение кому угодно
+    driveService = apiclient.discovery.build('drive', 'v3', http=httpAuth)
+    _ = driveService.permissions().create(
+        fileId=new_spreadsheet['spreadsheetId'],
+        body={'type': 'anyone', 'role': 'reader'},
+        fields='id'
+    ).execute()
 
-    SCOPES = [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive',
-    ]
-    
-    service = None
 
-    def __init__(self) -> None:
-        if os.path.exists('creds.json'):
-            creds = ServiceAccountCredentials(CREDENTIALS_FILE, scopes=self.SCOPES)
-            httpAuth = creds.authorize(httplib2.Http())
-            self.service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-    
+def connect_google_sheets_api(creds):
+    CREDENTIALS_FILE = creds
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_FILE,
+        ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive'])
+    httpAuth = credentials.authorize(httplib2.Http())
+    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+    return service, httpAuth
+
+
+def main():
+    service, http_auth = connect_google_sheets_api('creds.json')
+    info = 'New Event'
+    create_sheet(service, info, http_auth)
+
 
 if __name__ == '__main__':
-    gs = GSheet()
-    values = gs.service.spreadsheets().values().get(
-        spreadsheetId='1zZdLNLu6ntbXlS-A8pghN2TdORJQNPKvhVgvji8pMYI',
-        range='A1:B3',
-        majorDimension='COLUMNS'
-        ).execute()
-    pprint(values)
+    main()
+
+'''
+# Пример чтения файла
+values = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range='A1:E10',
+    majorDimension='COLUMNS'
+).execute()
+pprint(values)
+
+# Пример записи в файл
+values = service.spreadsheets().values().batchUpdate(
+    spreadsheetId=spreadsheet_id,
+    body={
+        "valueInputOption": "USER_ENTERED",
+        "data": [
+            {"range": "B3:C4",
+             "majorDimension": "ROWS",
+             "values": [["This is B3", "This is C3"], ["This is B4", "This is C4"]]},
+            {"range": "D5:E6",
+             "majorDimension": "COLUMNS",
+             "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]}
+
+        ]
+    }
+).execute()
+'''
