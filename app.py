4fc191da864fcf8b24c5f3edf6f02ee416794a16
Vov4ik3587 <galstyan.vova@gmail.com>
2022-11-25 01:29:25 +0700

    oh

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def start():        
+def start():
     return render_template("title.html")
+
 
 @app.route("/enroll")
 def subscribe():
     return render_template("subscribe.html")
 
+
 @app.route("/unsubscribe")
 def unsubscribe():
     return render_template("unsubscribe.html")
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- modules/db.py
+++ modules/db.py
@@ -1,27 +1,32 @@
 import psycopg2
 import sshtunnel
+from dotenv import load_dotenv
+import os
 
 
 def connect_db():
-    server = sshtunnel.SSHTunnelForwarder(('ssh.cloud.nstu.ru', 5115),
-                                          ssh_username='root',
-                                          ssh_password='ppm0du3j',
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+
+    server = sshtunnel.SSHTunnelForwarder((os.environ.get('SSH_DOMAIN'), 5115),
+                                          ssh_username=os.environ.get('SSH_USERNAME'),
+                                          ssh_password=os.environ.get('SSH_PASSWORD'),
                                           remote_bind_address=('localhost', 5432),
                                           local_bind_address=('localhost', 5432))
     server.start()
-
     conn = psycopg2.connect(
         host='localhost',
-        database='postgres',
-        user='postgres',
-        password='3587'
+        database=os.environ.get('DATABASE'),
+        user=os.environ.get('USER'),
+        password=os.environ.get('USER_PASSWORD')
     )
     # cursor = conn.cursor()
-    # cursor.execute('SELECT * FROM public.kok')
+    # cursor.execute('SELECT * FROM public.employees')
     # record = cursor.fetchall()
     # conn.commit()
     # cursor.close()
-    print(record)
+    # print(record)
     return conn
 
 
